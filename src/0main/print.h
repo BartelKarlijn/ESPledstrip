a98f04e891f22ef4eea31bfdaf1dc0cc95a48566
BartelKarlijn <legoreus@gmail.com>
2024-01-24 22:26:48 +0100

    size_t toevoegen

--- src/0main/print.h
+++ src/0main/print.h
@@ -13,6 +13,8 @@ void Print(uint16_t i)     {Print(String(i));}
 void Print(unsigned long i){Print(String(i));}
 void Print(int i)          {Print(String(i));}
 void Print(float i)        {Print(String(i));}
+void Print(size_t i)       {Print(String(i));}
+
 
 void Println(String TXT){
   #ifdef PRINTWEBSERIAL
@@ -26,3 +28,4 @@ void Println(uint16_t i)     {Println(String(i));}
 void Println(unsigned long i){Println(String(i));}
 void Println(int i)          {Println(String(i));}
 void Println(float i)        {Println(String(i));}
+void Println(size_t i)       {Println(String(i));}
