109e398b3cd6d73b8c192b6bb7e857c03fb59b89
BartelKarlijn <legoreus@gmail.com>
2024-01-28 18:34:23 +0100

    setupleds

--- src/0main/setupLed.h
+++ src/0main/setupLed.h
@@ -2,42 +2,11 @@ void setupLed(){
   // leds initialiseren met default
   for (size_t lednr = 0; lednr < LED_NR_ITEMS; lednr++) {
     
-//    ledsingle[lednr] = LED_DEFAULT[lednr];
-//    singleled.setPWM(lednr, 0, 0);     // alles uitzetten
+    ledsingle[lednr] = LED_DEFAULT[lednr];
+    pinMode(ledsingle[lednr].pin, OUTPUT);
+    ledcSetup(ledsingle[lednr].pwm_channel, PWM_FREQUENCY, PWM_RESOLUTION);  //eenmalig frequentie opzetten
+    ledcAttachPin(ledsingle[lednr].pin, ledsingle[lednr].pwm_channel);       //pin aan pwm channel hangen
   }
 
-    // pinMode(PIN_L1, OUTPUT);
-    // pinMode(PIN_L2, OUTPUT);
-    // pinMode(PIN_L3, OUTPUT);
-    // pinMode(PIN_L4, OUTPUT);
-    // pinMode(PIN_L5, OUTPUT);
-    // pinMode(PIN_R1, OUTPUT);
-    // pinMode(PIN_R2, OUTPUT);
-    // pinMode(PIN_R3, OUTPUT);
-    // pinMode(PIN_R4, OUTPUT);
-    // pinMode(PIN_R5, OUTPUT);
-    
-    // ledcSetup(PWM_CHANNEL_L1, PWM_FREQUENCY, PWM_RESOLUTION);
-    // ledcSetup(PWM_CHANNEL_L2, PWM_FREQUENCY, PWM_RESOLUTION);
-    // ledcSetup(PWM_CHANNEL_L3, PWM_FREQUENCY, PWM_RESOLUTION);
-    // ledcSetup(PWM_CHANNEL_L4, PWM_FREQUENCY, PWM_RESOLUTION);
-    // ledcSetup(PWM_CHANNEL_L5, PWM_FREQUENCY, PWM_RESOLUTION);
-    // ledcSetup(PWM_CHANNEL_R1, PWM_FREQUENCY, PWM_RESOLUTION);
-    // ledcSetup(PWM_CHANNEL_R2, PWM_FREQUENCY, PWM_RESOLUTION);
-    // ledcSetup(PWM_CHANNEL_R3, PWM_FREQUENCY, PWM_RESOLUTION);
-    // ledcSetup(PWM_CHANNEL_R4, PWM_FREQUENCY, PWM_RESOLUTION);
-    // ledcSetup(PWM_CHANNEL_R5, PWM_FREQUENCY, PWM_RESOLUTION);
-    
-    // ledcAttachPin(PIN_L1, PWM_CHANNEL_L1);
-    // ledcAttachPin(PIN_L2, PWM_CHANNEL_L2);
-    // ledcAttachPin(PIN_L3, PWM_CHANNEL_L3);
-    // ledcAttachPin(PIN_L4, PWM_CHANNEL_L4);
-    // ledcAttachPin(PIN_L5, PWM_CHANNEL_L5);
-    // ledcAttachPin(PIN_R1, PWM_CHANNEL_R1);
-    // ledcAttachPin(PIN_R2, PWM_CHANNEL_R2);
-    // ledcAttachPin(PIN_R3, PWM_CHANNEL_R3);
-    // ledcAttachPin(PIN_R4, PWM_CHANNEL_R4);
-    // ledcAttachPin(PIN_R5, PWM_CHANNEL_R5);
-
-    Println("Pins defined as output");
+  Println("Pins defined as output");
 }
